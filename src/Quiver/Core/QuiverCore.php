57bb8d04c04e5fbaef570f4d63444204df5bd479
W4X51M <wassimj0705@gmail.com>
2019-07-16 19:38:50 +0400

    Added Alerts
    Added the alerts feature to have in-game lobby messages

--- src/Quiver/Core/QuiverCore.php
+++ src/Quiver/Core/QuiverCore.php
@@ -10,25 +10,30 @@ use pocketmine\event\player\PlayerChatEvent;
 //chat filter
 use Quiver\ChatFilter\ChatFilter;
 use Quiver\ChatFilter\ChatFilterTask;
+//Alerts
+use Quiver\Alerts\AlertTask;
 
 class QuiverCore extends PluginBase implements Listener{
-	
+
 	public $users = [];
-	
+
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->getLogger()->info("QuiverCore enabled! Enjoy!");
-		
-		$this->filter = new ChatFilter();
-        $this->getScheduler()->scheduleRepeatingTask(new ChatFilterTask($this), 30);
-		
+
+	 	$this->getLogger()->info("QuiverCore enabled! Enjoy!");
+
+    $this->filter = new ChatFilter();
+    $this->getScheduler()->scheduleRepeatingTask(new ChatFilterTask($this), 30);
+
+		$this->getScheduler()->scheduleRepeatingTask(new AlertTask($this), 2000);
+
 		if($this->getConfig()->get('enabled') == false) {
             $this->setEnabled(false);
 			$this->getLogger()->info(TextFormat::RED . "PLUGIN HAS BEEN DISABLED IN THE CONFIG");
             return;
         }
 	}
-	
+
 		public function onPlayerChat(PlayerChatEvent $event) {
         if (array($event->getPlayer()->getDisplayName(), $this->users) && !$this->filter->check($event->getPlayer(), $event->getMessage())) {
             $event->setCancelled(true);
